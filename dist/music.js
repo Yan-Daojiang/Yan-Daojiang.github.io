7c506236d2af8bc72265c7bbbe8c55deb515ca81
Yan-Daojiang <1373836603@qq.com>
2019-09-19 12:48:16 +0800

    Site updated: 2019-09-19 12:48:10

--- dist/music.js
+++ dist/music.js
@@ -2,12 +2,40 @@ const ap = new APlayer({
   container: document.getElementById('aplayer'),
   fixed: true,
   autoplay: false,
+  listFolded: false,
   audio: [
     {
       name: "顽固",
       artist: '五月天',
       url: 'http://www.ytmp3.cn/down/49118.mp3',
       cover: 'https://ydj-image-1258588170.cos.ap-chengdu.myqcloud.com/myimage/wangu.jpg',
+    },
+    {
+      name: "如烟",
+      artist: '五月天',
+      url: 'https://ydj-image-1258588170.cos.ap-chengdu.myqcloud.com/myimage/ruyan.mp3',
+      cover: 'https://ydj-image-1258588170.cos.ap-chengdu.myqcloud.com/myimage/ruyan.jpg',
+    },
+    {
+      name: "干杯",
+      artist: '五月天',
+      url: 'https://ydj-image-1258588170.cos.ap-chengdu.myqcloud.com/myimage/ganbei.mp3',
+      cover: 'https://ydj-image-1258588170.cos.ap-chengdu.myqcloud.com/myimage/ganbei.jpg',
+    },
+    {
+      name: "如果我们不曾相遇",
+      artist: '五月天',
+      url: 'https://ydj-image-1258588170.cos.ap-chengdu.myqcloud.com/myimage/rgwmbcxy.mp3',
+      cover: 'https://ydj-image-1258588170.cos.ap-chengdu.myqcloud.com/myimage/rgwmbcxy.jpg',
+    },
+
+    {
+      name: "转眼",
+      artist: '五月天',
+      url: 'https://ydj-image-1258588170.cos.ap-chengdu.myqcloud.com/myimage/zhuanyan.jpg',
+      cover: 'https://ydj-image-1258588170.cos.ap-chengdu.myqcloud.com/myimage/zhuanyan.jpg',
     }
   ]
 })
+
+
